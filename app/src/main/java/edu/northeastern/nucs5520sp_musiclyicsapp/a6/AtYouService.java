7105e637d233932f01bfa59dda4fe60cf938d1e6
anhhoang <anhhoang0319@gmail.com>
2023-02-18 16:25:31 -0800

    upload assignment 6 version currencyEx_1.0

--- app/src/main/java/edu/northeastern/nucs5520sp_musiclyicsapp/a6/AtYouService.java
+++ app/src/main/java/edu/northeastern/nucs5520sp_musiclyicsapp/a6/AtYouService.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.json.JSONException;
@@ -23,65 +26,102 @@ import edu.northeastern.nucs5520sp_musiclyicsapp.R;
 public class AtYouService extends AppCompatActivity {
 
     TextView output;
-    EditText url;
+    EditText amount;
+    Spinner fromCurrency;
+    Spinner toCurrency;
+    ArrayAdapter adapter;
+
+    private String currencyFrom;
+    private String currencyTo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_at_you_service);
 
-        url = findViewById(R.id.editTextURL);
-        output = findViewById(R.id.textViewTitle);
-    }
+        amount = findViewById(R.id.editTextAmount);
+        output = findViewById(R.id.textViewAmountExchanged);
+        fromCurrency = findViewById(R.id.spinnerFrom);
+        toCurrency = findViewById(R.id.spinnerTo);
 
-    public void callWebserviceButtonHandler(View view){
-        PingWebServiceTask task = new PingWebServiceTask();
-        task.execute(url.getText().toString());
-    }
+        adapter = ArrayAdapter.createFromResource(this, R.array.currencies, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        fromCurrency.setAdapter(adapter);
+        toCurrency.setAdapter(adapter);
 
-    private class PingWebServiceTask extends AsyncTask<String, Integer, String[]> {
-
-        @Override
-        protected String[] doInBackground(String... params) {
-            String[] result = new String[2];
-            URL url = null;
-            try {
-                url = new URL(params[0]);
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("GET");
-                conn.setDoInput(true);
-                conn.connect();
-
-                InputStream inputStream = conn.getInputStream();
-                final String response = convertStreamToString(inputStream);
-
-                JSONObject jObject = new JSONObject(response);
-                String jTitle = jObject.getString("title");
-                String jBody = jObject.getString("body");
-                result[0] = jTitle;
-                result[1] = jBody;
-                return result;
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (JSONException e) {
-                e.printStackTrace();
+        fromCurrency.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                currencyFrom = parent.getItemAtPosition(position).toString();
             }
-            result[0] = "Something went wrong";
-            return(result);
-        }
 
-        @Override
-        protected void onPostExecute(String... s){
-            super.onPostExecute(s);
-            TextView output = (TextView) findViewById(R.id.textViewTitle);
-            output.setText(s[0]);
-        }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        toCurrency.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                currencyTo = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
-    private String convertStreamToString(InputStream is){
-        Scanner s = new Scanner(is).useDelimiter("\\A");
-        return s.hasNext() ? s.next().replace(",", ",\n") : "";
-    }
+//    public void callWebserviceButtonHandler(View view){
+//        PingWebServiceTask task = new PingWebServiceTask();
+//        task.execute(url.getText().toString());
+//    }
+//
+//    private class PingWebServiceTask extends AsyncTask<String, Integer, String[]> {
+//
+//        @Override
+//        protected String[] doInBackground(String... params) {
+//            String[] result = new String[2];
+//            URL url = null;
+//            try {
+//                url = new URL(params[0]);
+//                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//                conn.setRequestMethod("GET");
+//                conn.setDoInput(true);
+//                conn.connect();
+//
+//                InputStream inputStream = conn.getInputStream();
+//                final String response = convertStreamToString(inputStream);
+//
+//                JSONObject jObject = new JSONObject(response);
+//                String jTitle = jObject.getString("title");
+//                String jBody = jObject.getString("body");
+//                result[0] = jTitle;
+//                result[1] = jBody;
+//                return result;
+//            } catch (MalformedURLException e) {
+//                e.printStackTrace();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//            result[0] = "Something went wrong";
+//            return(result);
+//        }
+//
+//        @Override
+//        protected void onPostExecute(String... s){
+//            super.onPostExecute(s);
+//            TextView output = (TextView) findViewById(R.id.textViewAmountExchanged);
+//            output.setText(s[0]);
+//        }
+//    }
+//
+//    private String convertStreamToString(InputStream is){
+//        Scanner s = new Scanner(is).useDelimiter("\\A");
+//        return s.hasNext() ? s.next().replace(",", ",\n") : "";
+//    }
 }
