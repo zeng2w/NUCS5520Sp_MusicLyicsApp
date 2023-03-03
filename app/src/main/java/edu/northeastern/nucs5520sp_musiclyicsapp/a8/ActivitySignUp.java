7026fae3eb0bfca5d327337c3079b12ce3f659e6
Jerrybaoyilei <jerrybaoyilei@qq.com>
2023-03-03 15:55:42 -0500

    Set up button clicks for log in and sign up (switching in between these two)

--- app/src/main/java/edu/northeastern/nucs5520sp_musiclyicsapp/a8/ActivitySignUp.java
+++ app/src/main/java/edu/northeastern/nucs5520sp_musiclyicsapp/a8/ActivitySignUp.java
@@ -2,15 +2,26 @@ package edu.northeastern.nucs5520sp_musiclyicsapp.a8;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import edu.northeastern.nucs5520sp_musiclyicsapp.R;
 
-public class ActivitySignUp extends AppCompatActivity {
+public class ActivitySignUp extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int viewId = view.getId();
+        if (viewId == R.id.textViewLogIn) {
+            Intent intentLogIn = new Intent(ActivitySignUp.this, ActivityStickItToEm.class);
+            startActivity(intentLogIn);
+        }
     }
 }
--- app/src/main/java/edu/northeastern/nucs5520sp_musiclyicsapp/a8/ActivityStickItToEm.java
+++ app/src/main/java/edu/northeastern/nucs5520sp_musiclyicsapp/a8/ActivityStickItToEm.java
@@ -2,15 +2,26 @@ package edu.northeastern.nucs5520sp_musiclyicsapp.a8;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import edu.northeastern.nucs5520sp_musiclyicsapp.R;
 
-public class ActivityStickItToEm extends AppCompatActivity {
+public class ActivityStickItToEm extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stick_it_to_em);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int viewId = view.getId();
+        if (viewId == R.id.textViewSignUp) {
+            Intent intentSignUp = new Intent(ActivityStickItToEm.this, ActivitySignUp.class);
+            startActivity(intentSignUp);
+        }
     }
 }
