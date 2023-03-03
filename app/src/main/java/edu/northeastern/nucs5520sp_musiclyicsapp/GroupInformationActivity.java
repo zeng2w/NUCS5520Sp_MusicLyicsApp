b3f0d063b18da3fc0e6a5c3ae16802da09a9a6c2
Wanting Zeng <zeng.wan@northeastern.edu>
2023-03-02 21:25:19 -0500

    added the ABOUT button to show team info

---
+++ app/src/main/java/edu/northeastern/nucs5520sp_musiclyicsapp/GroupInformationActivity.java
@@ -0,0 +1,14 @@
+package edu.northeastern.nucs5520sp_musiclyicsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GroupInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_information);
+    }
+}
