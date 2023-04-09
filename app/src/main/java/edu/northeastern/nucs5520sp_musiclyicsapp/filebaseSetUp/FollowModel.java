6e2dee443fb83c98c625ceaef01733fcdd5dbe8f
Wanting Zeng <zeng.wan@northeastern.edu>
2023-04-09 18:33:24 -0400

    fixed some name issus

--- app/src/main/java/edu/northeastern/nucs5520sp_musiclyicsapp/filebaseSetUp/FollowModel.java
+++
@@ -1,8 +0,0 @@
-package edu.northeastern.nucs5520sp_musiclyicsapp.filebaseSetUp;
-
-public class FollowModel {
-    String followingUserId;
-    String followingUserName;
-    String followerUserId;
-    String followerUserName;
-}
