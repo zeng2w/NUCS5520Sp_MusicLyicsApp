8850218af7d9c6a1d17d4d35fa5e4fc57eb5ab3a
Jerrybaoyilei <jerrybaoyilei@qq.com>
2023-04-13 16:51:21 -0400

    Copied my experimental code from the personal app project for this class to the final project repo. Tested on using spotify API to obtain user authentication to their Spotify accounts and extract the song information given a playlistId, which can be extracted from the Spotify's playlist share link.

---
+++ app/src/main/java/edu/northeastern/nucs5520sp_musiclyicsapp/final_project/ActivitySpotify.java
@@ -0,0 +1,52 @@
+package edu.northeastern.nucs5520sp_musiclyicsapp.final_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import java.util.ArrayList;
+
+import edu.northeastern.nucs5520sp_musiclyicsapp.R;
+
+public class ActivitySpotify extends AppCompatActivity {
+
+    private ArrayList<SpotifySong> songsList;
+    private SongService songService;
+
+    // Credit: https://towardsdatascience.com/using-the-spotify-api-with-your-android-application-the-essentials-1a3c1bc36b9e
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spotify);
+
+        songService = new SongService(getApplicationContext());
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("SPOTIFY", 0);
+
+        getTracks();
+    }
+
+    // Whatever needs to be done with the songs obtained need to happen right below
+    // songsList = songService.getSongs(); (because of asynchronous nature of the request)
+    private void getTracks() {
+        songService.getSongsFromPlaylist("3gKA8ik8CVKljJsbIrem0b", 100, 0, new VolleyCallback() {
+            @Override
+            public void onSuccess(boolean finished) {
+                songsList = songService.getSongs();
+                // Songs in songsList can be used as input to the Genius API.
+                if (finished) {
+                    // These commented-out lines are used to test if songsList has all elements we want.
+//                    System.out.println("There are " + songsList.size() + " songs in the test playlist");
+//                    System.out.println("songList size is: " + songsList.size());
+//                    int count = 1;
+//                    for (Song song: songsList) {
+//                        System.out.println(count + " ==== " + "Song Title: " + song.getSongName());
+//                        count += 1;
+//                    }
+                }
+            }
+        });
+    }
+
+
+}
