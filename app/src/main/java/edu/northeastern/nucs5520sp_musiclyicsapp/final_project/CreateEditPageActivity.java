7626cf02938019112ead99ade72341863b18968b
Wanting Zeng <zeng.wan@northeastern.edu>
2023-04-14 09:45:48 -0400

    implement users create a new song lyrics, and added it to users library

--- app/src/main/java/edu/northeastern/nucs5520sp_musiclyicsapp/final_project/CreateEditPageActivity.java
+++ app/src/main/java/edu/northeastern/nucs5520sp_musiclyicsapp/final_project/CreateEditPageActivity.java
@@ -2,12 +2,24 @@ package edu.northeastern.nucs5520sp_musiclyicsapp.final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import edu.northeastern.nucs5520sp_musiclyicsapp.R;
+import edu.northeastern.nucs5520sp_musiclyicsapp.databinding.ActivityCreateEditPageBinding;
+import edu.northeastern.nucs5520sp_musiclyicsapp.final_project.model.SongModel;
 
 /*
 this is Create/Edit page which user can create/edit song title, artist, the original lyric, and
@@ -15,25 +27,69 @@ the translation of the lyric by themselves
  */
 public class CreateEditPageActivity extends AppCompatActivity {
 
-    EditText editPage_songName;
-    EditText editPage_artist;
-    EditText editPage_editLyric;
-    EditText editPage_editTranslate;
-    CheckBox editPage_checkBox;
-    Button editPage_buttonCancel;
-    Button editPage_buttonSave;
-
+    ActivityCreateEditPageBinding binding;
+    DatabaseReference databaseReference;
+    DatabaseReference databaseReferenceUsersLyricsLibrary;
+    DatabaseReference databaseReferenceSharedLyrics;
+    String currentUserEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_edit_page);
+        binding = ActivityCreateEditPageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        editPage_songName = findViewById(R.id.editPage_songName);
-        editPage_artist = findViewById(R.id.editPage_artist);
-        editPage_editLyric = findViewById(R.id.editPage_editLyric);
-        editPage_editTranslate = findViewById(R.id.editPage_editTranslate);
-        editPage_checkBox = findViewById(R.id.editPage_checkBox);
-        editPage_buttonCancel = findViewById(R.id.editPage_buttonCancel);
-        editPage_buttonSave = findViewById(R.id.editPage_buttonSave);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            currentUserEmail = user.getEmail();
+        }
+
+
+        databaseReferenceSharedLyrics = FirebaseDatabase.getInstance().getReference("shared_Lyrics");
+        databaseReferenceUsersLyricsLibrary = FirebaseDatabase.getInstance().getReference("users_Lyrics_Library");
+
+        // when click 'Save' button, if user select "Accessible to other users",
+        // the created new lyric will save to firebase realtime db --> both shared_Lyrics and users_Lyrics_Library
+        // otherwise, just save into db --> users_Lyrics_library
+        binding.editPageButtonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // create a new lyric
+                String newLyric_Name = binding.editPageSongName.getText().toString().replaceAll("[^a-zA-Z0-9]","");
+                String newLyric_Artist = binding.editPageArtist.getText().toString().replaceAll("[^a-zA-Z0-9]","");
+                String newLyric = binding.editPageEditLyric.getText().toString();
+                String newLyric_translation = binding.editPageEditTranslate.getText().toString();
+                // Log.d("-----song name:", newLyric_Name);
+                SongModel new_song = new SongModel(newLyric_Name, newLyric_Artist, newLyric, newLyric_translation, currentUserEmail);
+                if(TextUtils.isEmpty(newLyric_Name) || TextUtils.isEmpty(newLyric_Artist) || TextUtils.isEmpty(newLyric)){
+                    Toast.makeText(CreateEditPageActivity.this, "Input Empty, pleas fill out song Name/Artist/Lyrics", Toast.LENGTH_SHORT).show();
+                } else{
+                    // else will save the lyrics to db
+                    // if user select 'Accessible to other user', it will save into db --> both shared_Lyrics and users_Lyrics_Library
+                    if (binding.editPageCheckBox.isChecked()){
+                        databaseReferenceUsersLyricsLibrary.child(currentUserEmail.replaceAll("[^a-zA-Z0-9]",""))
+                                .child(newLyric_Name + newLyric_Artist).setValue(new_song);
+                        databaseReferenceSharedLyrics.child(newLyric_Name+ newLyric_Artist + currentUserEmail.replaceAll("[^a-zA-Z0-9]","")).setValue(new_song);
+                    } else {
+                        databaseReferenceUsersLyricsLibrary.child(currentUserEmail.replaceAll("[^a-zA-Z0-9]",""))
+                                .child(newLyric_Name + newLyric_Artist).setValue(new_song);
+                    }
+                    // when save successful, go to library page, then the new added song will show on library page
+                    startActivity(new Intent(CreateEditPageActivity.this, LibraryPageActivity.class));
+                    finish();
+                    //Toast.makeText(CreateEditPageActivity.this, "Saved Successful!", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
+        // when click 'Cancel' Button, lead user go back to library Page
+        binding.editPageButtonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // back to library page
+                startActivity(new Intent(CreateEditPageActivity.this, LibraryPageActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/edu/northeastern/nucs5520sp_musiclyicsapp/final_project/LibraryPageActivity.java
+++ app/src/main/java/edu/northeastern/nucs5520sp_musiclyicsapp/final_project/LibraryPageActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import edu.northeastern.nucs5520sp_musiclyicsapp.R;
+import edu.northeastern.nucs5520sp_musiclyicsapp.databinding.ActivityLibraryPageBinding;
 
 /*
 Library page that include recycler view of song title, artist, and lyric editor. At the bottom is
@@ -16,20 +19,28 @@ public class LibraryPageActivity extends AppCompatActivity {
 
     RecyclerView library_recyclerView;
 
+    ActivityLibraryPageBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_library_page);
+        binding = ActivityLibraryPageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         library_recyclerView = findViewById(R.id.library_recyclerView);
-
-
 
         /*
         set layout manager for the recycler view
          */
         library_recyclerView.setLayoutManager(new LinearLayoutManager(LibraryPageActivity.this));
+
+        binding.addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LibraryPageActivity.this, CreateEditPageActivity.class));
+                finish();
+            }
+        });
 
     }
 }
