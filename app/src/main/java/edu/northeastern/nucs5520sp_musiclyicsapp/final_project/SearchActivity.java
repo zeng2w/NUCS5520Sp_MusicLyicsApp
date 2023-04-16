b26175403b9306ab6badc33e8aa65aa0faf63cac
DonkeyBoy001 <zhouzhenzhou111@outlook.com>
2023-04-15 17:38:28 -0700

    update the layout version01

---
+++ app/src/main/java/edu/northeastern/nucs5520sp_musiclyicsapp/final_project/SearchActivity.java
@@ -0,0 +1,35 @@
+package edu.northeastern.nucs5520sp_musiclyicsapp.final_project;
+
+import static edu.northeastern.nucs5520sp_musiclyicsapp.R.id.local_search_toggle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
+
+import edu.northeastern.nucs5520sp_musiclyicsapp.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        ToggleButton myToggleButton = findViewById(local_search_toggle);
+
+        myToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // Handle the toggle state change here
+                if (isChecked) {
+                    // The toggle button is on
+                } else {
+                    // The toggle button is off
+                }
+            }
+        });
+
+    }
+}
