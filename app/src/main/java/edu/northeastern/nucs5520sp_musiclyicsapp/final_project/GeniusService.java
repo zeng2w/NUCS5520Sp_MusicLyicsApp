27f4a9d2d0d461416e55608179c5a9a75b8ecf8e
Jerrybaoyilei <jerrybaoyilei@qq.com>
2023-04-17 19:31:09 -0400

    Added a method to extract the actual lyrics from a lyrics url returned from Genius API's request object. Also tried Selenium but it was too slow. Kept getting 403 forbidden at first with Jsoup before setting up user agent correctly.

--- app/src/main/java/edu/northeastern/nucs5520sp_musiclyicsapp/final_project/GeniusService.java
+++ app/src/main/java/edu/northeastern/nucs5520sp_musiclyicsapp/final_project/GeniusService.java
@@ -9,16 +9,14 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -30,72 +28,102 @@ import edu.northeastern.nucs5520sp_musiclyicsapp.final_project.model.SpotifySong
 public class GeniusService {
 
     private ArrayList<GeniusSong> SongsWithLyricsList;
-    private final RequestQueue queue;
+//    private final RequestQueue queue;
     private String GENIUS_ACCESS_TOKEN = "sd5_JeeFfCuttmEMQCQqcbPof3Q9kP6GCqzyhSX4Q8Eit9jClmicz77-FHOXKam2";
 
-    public GeniusService(Context context) {
+//    public GeniusService(Context context) {
+//        this.SongsWithLyricsList = new ArrayList<>();
+//        queue = Volley.newRequestQueue(context);
+//    }
+
+    public GeniusService() {
         this.SongsWithLyricsList = new ArrayList<>();
-        queue = Volley.newRequestQueue(context);
     }
 
     public ArrayList<GeniusSong> getSongsWithLyricsList() {
         return SongsWithLyricsList;
     }
 
-    public void getLyricsForSong(String songName, ArrayList<String> artists) {
-
-
-            // Set up the search content
-        String artistsLongString = String.join(" ", artists);
-
-        // Build the API link. Remove white spaces at then ends of endpoint and replace white
-        // spaces with %20.
-        String endpoint = "api.genius.com/search?q=" + songName + artistsLongString;
-        endpoint = endpoint.trim();
-        endpoint = endpoint.replace(" ", "%20");
-
-        // Create the request.
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                (Request.Method.GET, endpoint, null, response -> {
-                    JSONArray songsJsonArray = response.optJSONArray("hits");
-                    // We choose the first song among all songs returned from the search, if
-                    // the search result is not empty.
-                    if (!songsJsonArray.isNull(0)) {
-                        JSONObject topSong = songsJsonArray.optJSONObject(0);
-                        String lyricsUrl = topSong.optJSONObject("result").optString("url");
-
-                    }
-
-
-
-
-        }, error -> {
-
-        }) {
-            @Override
-            public Map<String, String> getHeaders() {
-                Map<String, String> headers = new HashMap<>();
-                String token = GENIUS_ACCESS_TOKEN;
-                String auth = "Bearer " + token;
-                headers.put("Authorization", auth);
-                return headers;
-            }
-        };
-
-        queue.add(jsonObjectRequest);
-    }
+//    public void getLyricsForSong(String songName, ArrayList<String> artists) {
+//
+//
+//            // Set up the search content
+//        String artistsLongString = String.join(" ", artists);
+//
+//        // Build the API link. Remove white spaces at then ends of endpoint and replace white
+//        // spaces with %20.
+//        String endpoint = "api.genius.com/search?q=" + songName + artistsLongString;
+//        endpoint = endpoint.trim();
+//        endpoint = endpoint.replace(" ", "%20");
+//
+//        // Create the request.
+//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+//                (Request.Method.GET, endpoint, null, response -> {
+//                    JSONArray songsJsonArray = response.optJSONArray("hits");
+//                    // We choose the first song among all songs returned from the search, if
+//                    // the search result is not empty.
+//                    if (!songsJsonArray.isNull(0)) {
+//                        JSONObject topSong = songsJsonArray.optJSONObject(0);
+//                        String lyricsUrl = topSong.optJSONObject("result").optString("url");
+//
+//                    }
+//
+//
+//
+//
+//        }, error -> {
+//
+//        }) {
+//            @Override
+//            public Map<String, String> getHeaders() {
+//                Map<String, String> headers = new HashMap<>();
+//                String token = GENIUS_ACCESS_TOKEN;
+//                String auth = "Bearer " + token;
+//                headers.put("Authorization", auth);
+//                return headers;
+//            }
+//        };
+//
+//        queue.add(jsonObjectRequest);
+//    }
 
     /**
      * Scrape the lyrics from the genius.com link to the lyrics page of a song.
      * Credit: https://jsoup.org/
+     * (Below helped me with defining user agent.
+     * Credit: https://itecnote.com/tecnote/jsoup-error-403-when-trying-to-read-the-contents-of-a-directory-on-the-website
      */
     public String getLyricsFromUrl(String lyricsUrl) throws IOException {
-        // Use Jsoup to get the
-        Document doc = Jsoup.connect(lyricsUrl).get();
-        Element
 
+        // Credit: https://itecnote.com/tecnote/jsoup-error-403-when-trying-to-read-the-contents-of-a-directory-on-the-website/
+        // !!!!! Thanks to this I am not getting 403 forbidden.
+        String userAgent = "Mozilla/5.0";
 
+        Connection connection = Jsoup.connect(lyricsUrl).userAgent(userAgent);
+
+        Document doc = connection.get();
+
+        Element lyricsContainer = doc.selectFirst("div[data-lyrics-container='true']");
+        assert lyricsContainer != null;
+        Elements aElements = lyricsContainer.select("a");
+        StringBuilder lyricsBuilder = new StringBuilder();
+        for (Element aElement: aElements) {
+            Element spanElement = aElement.selectFirst("span");
+            assert spanElement != null;
+            lyricsBuilder.append(spanElement.text().trim()).append("\n");
+        }
+        String lyrics = lyricsBuilder.toString();
+        lyrics = lyrics.replaceAll("\\s(?=[A-Z])", "\n");
+        return lyrics;
     }
+
+    public static void main(String[] args) throws IOException {
+        GeniusService service = new GeniusService();
+
+        String testLyrics = service.getLyricsFromUrl("https://genius.com/Coldplay-yellow-lyrics");
+        System.out.println("Yellow lyrics: \n" + testLyrics);
+    }
+
 
 
 
