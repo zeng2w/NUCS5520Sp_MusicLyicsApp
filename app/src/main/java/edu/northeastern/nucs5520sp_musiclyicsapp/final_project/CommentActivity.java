218b0baeb03102622b34b646867c5bca0edc85e8
DonkeyBoy001 <zhouzhenzhou111@outlook.com>
2023-04-16 22:28:26 -0700

    modifying the CommentActivity to add adapter(not finish) and add comment funcion below each comment in comment.xml

--- app/src/main/java/edu/northeastern/nucs5520sp_musiclyicsapp/final_project/CommentActivity.java
+++ app/src/main/java/edu/northeastern/nucs5520sp_musiclyicsapp/final_project/CommentActivity.java
@@ -1,16 +1,31 @@
 package edu.northeastern.nucs5520sp_musiclyicsapp.final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import edu.northeastern.nucs5520sp_musiclyicsapp.R;
+import edu.northeastern.nucs5520sp_musiclyicsapp.databinding.ActivityCommentBinding;
+import edu.northeastern.nucs5520sp_musiclyicsapp.databinding.ActivityLibraryPageBinding;
+import edu.northeastern.nucs5520sp_musiclyicsapp.final_project.adapters.CommentAdapter;
 
 public class CommentActivity extends AppCompatActivity {
+
+    RecyclerView comment_recycleView;
+    ActivityCommentBinding binding;
+    CommentAdapter commentAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comment);
+
+        binding = ActivityCommentBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // set adapter to recycler view
+        commentAdapter = new CommentAdapter(this);
+//        binding.commentRecycleView.setAdapter(commentAdapter);
     }
 }
