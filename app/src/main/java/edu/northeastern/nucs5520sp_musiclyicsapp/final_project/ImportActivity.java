e67421a978b1399e98d1966c75ef1cfc75e630f7
Jerrybaoyilei <jerrybaoyilei@qq.com>
2023-04-23 16:30:16 -0400

    ActivityImport.java (now ImportActivity.java to match with dev branch name) restored.

--- app/src/main/java/edu/northeastern/nucs5520sp_musiclyicsapp/final_project/ImportActivity.java
+++ app/src/main/java/edu/northeastern/nucs5520sp_musiclyicsapp/final_project/ImportActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import edu.northeastern.nucs5520sp_musiclyicsapp.R;
 import edu.northeastern.nucs5520sp_musiclyicsapp.databinding.ActivityImportBinding;
@@ -12,18 +16,46 @@ public class ImportActivity extends AppCompatActivity {
 
     ActivityImportBinding binding;
 
+    private EditText editTextLink;
+
+    private Button buttonImport;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityImportBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+//        binding.navBarView.setOnItemSelectedListener(item -> {
+//            switch (item.getItemId()){
+//                case R.id.navBar_library:
+//                    startActivity(new Intent(ActivityImport.this, LibraryPageActivity.class));
+//                    break;
+//                case R.id.navBar_user:
+//                    startActivity(new Intent(ActivityImport.this, UserPageActivity.class));
+//                    break;
+//            }
+//            return true;
+//        });
+        editTextLink = findViewById(R.id.spotify_list_edittext);
+
+        buttonImport = findViewById(R.id.import_button);
+
+        buttonImport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService();
+            }
+        });
     }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        startActivity(new Intent(ImportActivity.this,LibraryPageActivity.class));
-        finish();
+    // Credit: https://youtu.be/FbpD5RZtbCc
+    public void startService() {
+        String sharedPlaylistLink = editTextLink.getText().toString().trim();
+
+        Intent serviceIntent = new Intent(this, ImportService.class);
+        serviceIntent.putExtra("Shared Playlist Link", sharedPlaylistLink);
+
+        startService(serviceIntent);
     }
 }
