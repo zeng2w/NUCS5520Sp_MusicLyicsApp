96b4a3fae137cb87e45fe9fb742bc36ebd5cb0af
Jerrybaoyilei <jerrybaoyilei@qq.com>
2023-04-24 22:06:37 -0400

    Attempted at changing Service to JobIntentService but did not work. Fixed crashing when hitting back button at ActivityIntentResult. Added notification if user hasn't authorized Spotify or Spotify's token expires.

--- app/src/main/java/edu/northeastern/nucs5520sp_musiclyicsapp/final_project/ActivitySpotify.java
+++ app/src/main/java/edu/northeastern/nucs5520sp_musiclyicsapp/final_project/ActivitySpotify.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -77,12 +78,7 @@ public class ActivitySpotify extends AppCompatActivity {
                             }
                         });
                         // Cope with Genius API's rate limit for search, which is 1 per second.
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-
+                        SystemClock.sleep(1000);
                     }
 
                 }
